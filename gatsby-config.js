b5a2a944b4dfee178b8d3154b7be070707c21149
Arthur <arthur.kehrwald@gmail.com>
2023-05-19 15:23:37 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Sandbox`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
