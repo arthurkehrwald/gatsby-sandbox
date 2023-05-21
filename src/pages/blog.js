5dbaf15c8cd15e5da1fcacc295268e3a0a6b2698
Arthur <arthur.kehrwald@gmail.com>
2023-05-21 13:19:18 +0200

    Display blog post titles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,34 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import { graphql } from 'gatsby'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="Blog">
-            <p>My posts:</p>
+            <ul>
+            {
+                data.allFile.nodes.map(node => (
+                    <li key={node.name}>
+                        {node.name}
+                    </li>
+                ))
+            }
+            </ul>
         </Layout>
     )
 }
 
-export const Head = () => <Seo title="Blog"/>
+export const query = graphql`
+    query {
+        allFile {
+            nodes {
+                name
+            }
+        }
+    }
+`
+
+export const Head = () => <Seo title="Blog" />
 
 export default BlogPage
